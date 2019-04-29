a3233589a7b58bf4d44e769cbcd5a87f54ed4003
Nathan Getty <ngetty@wawanesa.com>
2019-04-29 15:04:50 -0500

    updated

---
+++ README.md
@@ -0,0 +1,7 @@
+1. Download the repo
+
+git clone <url>
+
+2. Run the deploys script
+
+./deploy.sh
--- deploy.sh
+++ deploy.sh
@@ -11,13 +11,10 @@ if [ ! -d "$SCRIPT_DIR" ]; then
     echo "$SCRIPT_DIR does not exist, creating"
 fi
 
-# CHECKING FOR JQ
-if [ ! -f "$SCRIPT_DIR/jq" ]; then
-    echo "JQ was not found downloading now"
-    curl -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-win64.exe --output $SCRIPT_DIR/jq
-fi
 
 echo "Replacing BashRC"
 cp bootstraps/.bashrc $HOME/.bashrc
 
 echo "Home Dir: $HOME"
+
+bash
